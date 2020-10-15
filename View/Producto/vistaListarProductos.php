44b2127d531f7a5d9fceb71c01a27846ff2fa58d
unknown <jperez@clinicalaluz.com.pe>
2020-10-14 19:00:03 -0500

    11vo cambio

--- View/Producto/vistaListarProductos.php
+++ View/Producto/vistaListarProductos.php
@@ -21,6 +21,19 @@
             <div class="box box-success">
                 <!-- /.box-header -->
                 <div class="box-body">
+                    <div class="form-group">
+                        <div class="col-lg-6">
+                            <div class="input-group">
+                                <input type="text" class="global_filter form-control" id="global_filter" placeholder="Ingrese el dato a buscar">
+                                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            </div>
+                        </div>
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-2">
+                            <button class="btn btn-success" style="width: 100%;" onclick="AbrirModalRegistro()"><i class="glyphicon glyphicon-plus"></i> Nuevo Producto</button>
+                        </div>
+                    </div>
+                    <br><br>
                     <table id="tabla_productos" class="display responsive nowrap" style="width:100%">
                         <thead>
                             <tr>
@@ -58,6 +71,65 @@
     <!-- /.row -->
 </section>
 <!-- /.content -->
+
+<!-- modal Agregar -->
+<div class="modal fade" id="modal_registro" role="dialog">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title">Registro de Productos</h3>        
+      </div>
+      <div class="modal-body">
+        <div class="col-lg-12">
+            <label for="">ID Usuario</label>
+            <input type="text" class="form-control" id="txtid" value="0" disabled>
+        </div><br>
+        <div class="col-lg-12">
+            <label for="">Nombres Completos</label>
+            <input type="text" class="form-control" id="txtnombres" placeholder="Ingrese sus nombres completos">
+        </div><br>
+      </div>
+      <div class="modal-footer">
+        <br><button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+        <button type="button" class="btn btn-success" onclick="RegistrarProducto()"><i class="fa fa-check-square-o"></i> Guardar</button>
+      </div><br>
+    </div>
+  </div>
+</div>
+<!-- /.modal Agregar -->
+
+<!-- modal Editar -->
+<div class="modal fade" id="modal_editar" role="dialog">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h3 class="modal-title">Editar Usuario</h3>        
+      </div>
+      <div class="modal-body">
+        <div class="col-lg-12">
+            <input type="hidden" class="form-control" id="txteid">
+            <label for="">Nombres Completos</label>
+            <input type="text" class="form-control" id="txtenombres" disabled>
+        </div><br>
+        <div class="col-lg-12">
+            <label for="">Correo Electronico</label>
+            <input type="email" class="form-control" id="txteemail">
+        </div><br>
+        <div class="col-lg-12">
+            <label for="">Direccion</label>
+            <input type="text" class="form-control" id="txtedireccion"><br>        
+        </div>
+      </div>
+      <div class="modal-footer">
+        <br><button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+        <button type="button" class="btn btn-success" onclick="ModificarUsuario()"><i class="fa fa-check-square-o"></i> Editar</button>
+      </div><br>
+    </div>
+  </div>
+</div>
+<!-- /.modal Editar -->
 
 <script>
     $(document).ready(function() {
--- js/producto.js
+++ js/producto.js
@@ -1,5 +1,7 @@
+var table = "";
+
 function ListarProductos(){
-    var table = $("#tabla_productos").DataTable({
+    table = $("#tabla_productos").DataTable({
         "ordering": false,
         "paging": false,
         "searching": { "regex": true },
@@ -39,10 +41,211 @@ function ListarProductos(){
                 }
             },
             {"data": "imagen"},
-            {"defaultContent": "<button style='font-size:13px;' type='button' class='editar btn btn-primary'><i class='fa fa-edit'></i></button>"}
+            {"defaultContent": "<button style='font-size:13px;' type='button' class='editar btn btn-warning'>"
+                                    +"<i class='fa fa-edit'></i></button>&nbsp"
+                                +"<button style='font-size:13px;' type='button' class='activar btn btn-primary'"
+                                    +" id='btnactivar'><i class='fa fa-check'></i></button>&nbsp"
+                                +"<button style='font-size:13px;' type='button' class='desactivar btn btn-danger'"
+                                    +" id='btndesactivar'><i class='fa fa-trash'></i></button>"
+            }
         ],
 
         "language": idioma_espanol,
         select: true
+    });
+
+    document.getElementById("tabla_productos_filter").style.display = "none";
+
+    $('input.global_filter').on('keyup click', function() {
+        filterGlobal();
+    });
+
+    $('input.global_filter').on('keyup click', function() {
+        filterColumn( $(this).parents('tr').attr('data-column') );
+    });
+}
+
+function filterGlobal(){
+    $('#tabla_productos').DataTable().search(
+        $('#global_filter').val(),
+    ).draw();
+}
+
+function AbrirModalRegistro(){
+    $('#modal_registro').modal({backdrop:'static', keyboard:false})
+    $('#modal_registro').modal('show');
+}
+
+//falta editar desde aqui------------------------------------------------------------------
+function RegistrarUsuario(){
+    var nom = $('#txtnombres').val();
+    var dir = $('#txtdireccion').val();
+    var email = $('#txtemail').val();
+    var pass = $('#txtpass').val();
+    var rpass = $('#txtrpass').val();
+
+    if(nom.length == 0 || dir.length == 0 || email.length == 0 || pass.length == 0 || rpass.length == 0){
+        return Swal.fire("Mensaje de Advertencia", "Debe de llenar los campos vacios", "warning")
+    } 
+
+    if(pass != rpass){
+        return Swal.fire("Mensaje de Advertencia", "Las contraseñas deben coincidir...", "warning")
+    }
+
+    $.ajax({
+        url: '../Controller/Usuario/RegistrarUsuario.php',
+        type: 'POST',
+        data: {
+            nombres: nom,
+            direccion: dir,
+            email: email,
+            contra: pass
+        }
+    }).done(function(resp) {
+        if(resp > 0){
+            if(resp == 1){
+                $('#modal_registro').modal('hide');
+                Swal.fire("Mensaje de Confirmacion", "Datos almacenados correctamente!", "success")
+                .then( ( value ) => {
+                    LimpiarRegistro();
+                    table.ajax.reload();
+                });
+            } else {
+                return Swal.fire("Mensaje de Advertencia", "Lo sentimos, el usuario ya se encuentra registrado.", "warning");
+            }
+        } else {
+            Swal.fire("Mensaje de Error", "Lo sentimos, no se pudo guardar los datos.", "error");
+        }
+    });
+}
+
+function LimpiarRegistro(){
+    $('#txtnombres').val("");
+    $('#txtdireccion').val("");
+    $('#txtemail').val("");
+    $('#txtpass').val("");
+    $('#txtrpass').val("");
+}
+
+$('#tabla_usuarios').on('click', '.desactivar', function() {
+    var data = table.row($(this).parents('tr')).data();
+    if(table.row(this).child.isShown()){
+        var data = table.row(this).data();
+    }
+    //alert(data.idUsu);
+    Swal.fire({
+        title: 'Esta seguro en deshabilitar al usuario?',
+        text: "Una vez hecho esto, el usuario no tendra acceso al sistema!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          /*Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )*/
+          ModificarEstadoUsuario(data.idUsu, 'I');
+        }
+      })
+});
+
+$('#tabla_usuarios').on('click', '.activar', function() {
+    var data = table.row($(this).parents('tr')).data();
+    if(table.row(this).child.isShown()){
+        var data = table.row(this).data();
+    }
+    //alert(data.idUsu);
+    Swal.fire({
+        title: 'Esta seguro en habilitar al usuario?',
+        text: "Una vez hecho esto, el usuario tendra acceso al sistema!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          /*Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )*/
+          ModificarEstadoUsuario(data.idUsu, 'A');
+        }
+      })
+});
+
+function ModificarEstadoUsuario(id,estado){
+    var mensaje = "";
+
+    if(estado == 'A'){
+        mensaje = "habilitó";
+    } else {
+        mensaje = "deshabilitó";
+    }
+
+    $.ajax({
+        url: '../Controller/Usuario/ModificarEstadoUsuario.php',
+        type: 'POST',
+        data: {
+            id: id,
+            estado: estado
+        }
+    }).done(function(resp) {
+        if(resp > 0){
+            Swal.fire("Mensaje de Confirmacion", "El usuario se " + mensaje + " con exito!", "success")
+                .then( ( value ) => {
+                    table.ajax.reload();
+                });
+        } 
+    });
+}
+
+$('#tabla_usuarios').on('click', '.editar', function() {
+    var data = table.row($(this).parents('tr')).data();
+    if(table.row(this).child.isShown()){
+        var data = table.row(this).data();
+    }
+
+    $('#modal_editar').modal({backdrop:'static', keyboard:false})
+    $('#modal_editar').modal('show');    
+
+    $('#txteid').val(data.idUsu);
+    $('#txtenombres').val(data.nomUsu);
+    $('#txtedireccion').val(data.dirUsu);
+    $('#txteemail').val(data.emailUsu);
+});
+
+function ModificarUsuario(){
+    var id = $('#txteid').val();
+    var dir = $('#txtedireccion').val();
+    var email = $('#txteemail').val();
+
+    if(dir.length == 0 || email.length == 0){
+        return Swal.fire("Mensaje de Advertencia", "Debe de llenar los campos vacios", "warning")
+    }
+
+    $.ajax({
+        url: '../Controller/Usuario/ModificarUsuario.php',
+        type: 'POST',
+        data: {
+            id: id,
+            direccion: dir,
+            email: email
+        }
+    }).done(function(resp) {
+        if(resp > 0){
+            $('#modal_editar').modal('hide');
+            Swal.fire("Mensaje de Confirmacion", "Datos modificados correctamente!", "success")
+            .then( ( value ) => {
+                table.ajax.reload();
+            });
+        } else {
+            Swal.fire("Mensaje de Error", "Lo sentimos, no se pudo modificar los datos.", "error");
+        }
     });
 }
